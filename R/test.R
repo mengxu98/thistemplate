2ea9a7509c2883b41757a3c6bcb19a2c7fae3cb0
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-04-28 15:39:04 +0800

    Update

--- R/test.R
+++ R/test.R
@@ -2,37 +2,8 @@
 #'
 #' @export
 #' @examples
-#'
 #' test()
+#'
 test <- function() {
-  print("pof")
-}
-#' Test function
-#'
-#' @export
-#' @examples
-#'
-#' test2()
-test2 <- function() {
-  print("pof")
-}
-
-#' Test function
-#'
-#' @export
-#' @examples
-#'
-#' test3()
-test3 <- function() {
-  print("pof")
-}
-
-#' Test function
-#'
-#' @export
-#' @examples
-#'
-#' test4()
-test4 <- function() {
-  print("pof")
+  print("testing")
 }
