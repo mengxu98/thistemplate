0a087c62ffad820678e706522582e0e0324c504d
Meng Xu <86040828+mengxu98@users.noreply.github.com>
2025-05-02 11:26:19 +0800

    add `use_mxtemplate` function

---
+++ R/use_mxtemplate.R
@@ -0,0 +1,129 @@
+#' Use mxtemplate theme for pkgdown
+#'
+#' This function helps you set up the mxtemplate theme for your package's pkgdown site.
+#' It will:
+#' 1. Create or update _pkgdown.yml with the necessary template configuration
+#' 2. Add mxtemplate to the Config/Needs/website field in DESCRIPTION
+#'
+#' @param github_url Optional. The GitHub URL of your package. If provided, will be added to the navbar.
+#' @param author Optional. The author name. If not provided, will be extracted from DESCRIPTION.
+#' @export
+use_mxtemplate <- function(
+    github_url = NULL,
+    author = NULL) {
+  root_pkgdown <- "_pkgdown.yml"
+  if (file.exists(root_pkgdown)) {
+    pkgdown_path <- root_pkgdown
+  } else {
+    pkgdown_files <- list.files(
+      path = ".",
+      pattern = "_pkgdown\\.yml$",
+      recursive = TRUE,
+      full.names = TRUE
+    )
+
+    if (length(pkgdown_files) == 0) {
+      pkgdown_path <- root_pkgdown
+    } else {
+      inst_pkgdown <- pkgdown_files[grepl("inst/pkgdown", pkgdown_files)]
+      if (length(inst_pkgdown) > 0) {
+        pkgdown_path <- inst_pkgdown[1]
+      } else {
+        pkgdown_path <- pkgdown_files[1]
+        warning(
+          sprintf(
+            "No _pkgdown.yml found in root or inst/pkgdown. Using %s instead.",
+            pkgdown_path
+          )
+        )
+      }
+    }
+  }
+
+  if (is.null(author)) {
+    author_info <- desc::desc_get_author()
+    author <- gsub("^([^<]+).*$", "\\1", author_info)
+    author <- trimws(author)
+  }
+
+  if (is.null(github_url)) {
+    github_url <- desc::desc_get_urls()
+    if (length(github_url) > 0) {
+      github_url <- github_url[grepl("github", github_url)]
+      if (length(github_url) > 0) {
+        github_url <- github_url[1]
+      }
+    } else {
+      warning("No GitHub URL found in DESCRIPTION")
+      github_url <- NULL
+    }
+  }
+
+  if (!file.exists(pkgdown_path)) {
+    yaml_content <- list(
+      template = list(
+        package = "mxtemplate",
+        bootstrap = 5,
+        `light-switch` = TRUE
+      )
+    )
+
+    yaml_content$navbar <- list(
+      structure = list(
+        left = c("articles", "reference", "tutorials", "news"),
+        right = c("github", author, "lightswitch")
+      ),
+      components = list(
+        github = list(
+          icon = "fab fa-github fa-lg",
+          href = github_url
+        )
+      )
+    )
+
+    yaml::write_yaml(yaml_content, pkgdown_path)
+    message("Created _pkgdown.yml with mxtemplate configuration")
+  } else {
+    yaml_content <- yaml::read_yaml(pkgdown_path)
+
+    yaml_content$template <- list(
+      package = "mxtemplate",
+      bootstrap = 5,
+      `light-switch` = TRUE
+    )
+
+    if (is.null(yaml_content$navbar)) {
+      yaml_content$navbar <- list()
+    }
+
+    if (is.null(yaml_content$navbar$structure)) {
+      yaml_content$navbar$structure <- list(
+        left = c("articles", "reference", "tutorials", "news"),
+        right = c("github", author, "lightswitch")
+      )
+    }
+
+    if (is.null(yaml_content$navbar$components)) {
+      yaml_content$navbar$components <- list()
+    }
+    yaml_content$navbar$components$github <- list(
+      icon = "fab fa-github fa-lg",
+      href = github_url
+    )
+
+    yaml::write_yaml(yaml_content, pkgdown_path)
+    message(sprintf("Updated %s with mxtemplate configuration", pkgdown_path))
+  }
+
+  desc_path <- "DESCRIPTION"
+  if (!file.exists(desc_path)) {
+    stop("DESCRIPTION file not found")
+  }
+
+  desc <- desc::description$new(desc_path)
+  desc$set("Config/Needs/website", "mengxu98/mxtemplate")
+  desc$write()
+  message("Updated DESCRIPTION with mxtemplate dependency")
+
+  invisible(TRUE)
+}
